f55dad6b0c373391bb580858b4c28ab8ea6561b3
Bermi Ferrer <bermi@bermilabs.com>
2017-04-24 17:43:41 +0200

    Adding tests for garbage collection

--- test/gc.test.js
+++ test/gc.test.js
@@ -1,56 +1,58 @@
 /* eslint no-sync: 0, init-declarations: 0 */
 
 const path = require("path");
-const cacheDir = path.normalize(`${__dirname}/fixtures/disk-memoizer`);
-
-process.env.NODE_ENV = "test";
-
 const gc = require("../lib/gc");
+const getCachePath = require("../lib/disk_memoizer").getCachePath;
 const fs = require("fs");
 const assert = require("assert");
+const cacheDir = path.normalize(`${__dirname}/fixtures/`);
 
 
 describe("When performing gc", () => {
-  const startDate = new Date();
   let gcInterval;
-  const tmpBasePath =
-    path.normalize(`${__dirname}/fixtures/prefix`);
+
+  const startDate = new Date();
+  const aPath = getCachePath("a", cacheDir);
+  const bPath = getCachePath("b", cacheDir);
+  const cPath = getCachePath("c", cacheDir);
+  const ignoredPath = path.normalize(`${aPath}.ignored`);
 
   before(() => {
 
     const pastDate = new Date(startDate - (1000 * 60 * 60 * 2));
 
-    fs.writeFileSync(`${tmpBasePath}-a`, "");
+    fs.writeFileSync(aPath, "a");
 
-    // Only files with prefix- should be considered
-    fs.utimesSync(path.normalize(`${__dirname}/fixtures/please-ignore`),
-      pastDate, pastDate);
+    // Only files with the .cache suffix should be considered
+    fs.utimesSync(ignoredPath, pastDate, pastDate);
 
-    fs.utimesSync(`${tmpBasePath}-a`, pastDate, pastDate);
-    fs.utimesSync(`${tmpBasePath}-b`, startDate, startDate);
-    fs.utimesSync(`${tmpBasePath}-c`, startDate, startDate);
+    fs.utimesSync(aPath, pastDate, pastDate);
+    fs.utimesSync(bPath, startDate, startDate);
+    fs.utimesSync(cPath, startDate, startDate);
   });
 
-  after(() => {
-    clearInterval(gcInterval);
-    fs.writeFileSync(`${tmpBasePath}-a`, "");
+  after((done) => {
+    console.log(aPath);
+    fs.writeFile(aPath, "a", done);
   });
 
   it("should list files to delete", (done) => {
-    gc.getFilesToDelete(`${__dirname}/fixtures/`, {maxAge: "1h"},
+    gc.getFilesToDelete({
+      cacheDir,
+      maxAge: "1h"
+    },
       (err, files) => {
         if (err) {
           throw err;
         }
         const paths = Object.keys(files);
 
-        // Only files with prefix- should be considered
+        // Only files with the .cache extension should be considered
         paths.forEach((path) => {
-          assert(path.includes("prefix-"),
-            "Unexpected file name");
+          assert(path.match(/\.cache$/), "Unexpected file name");
         });
 
-        assert(paths[0].includes("prefix-a"), "Didn't find file");
+        assert.equal(paths[0], aPath, "Didn't find file");
         assert(paths.length === 1, "Number of paths to delete didn't match");
         assert(files[paths[0]] instanceof Date, "Not a Date instance");
         assert(files[paths[0]] <= startDate, "startDate larger than expected");
@@ -69,10 +71,11 @@ describe("When performing gc", () => {
     });
 
     setTimeout(() => {
-      fs.stat(`${tmpBasePath}-a`, (err, stats) => {
+      fs.stat(aPath, (err, stats) => {
         assert(err instanceof Error, "Didn't return an error");
         assert(err.message.includes("ENOENT"), "Invalid error");
         assert(!stats, "Didn't expect stats to be set");
+        clearInterval(gcInterval);
         done();
       });
 
